217fbb4dbe0442a50390793b39a9a23e1948572b
DEnFUrt <musorov@gmail.com>
2020-03-05 21:10:56 +0500

    fifth lesson

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+//CONSTANTS
+
+const btnStart = document.querySelector('#start');
+
+const resultTable = document.querySelector('.result-table'),
+    budgetValue = resultTable.querySelector('.budget-value'),
+    levelValue = resultTable.querySelector('.level-value'),
+    expensesValue = resultTable.querySelector('.expenses-value'),
+    optionalexpensesValue = resultTable.querySelector('.optionalexpenses-value'),
+    incomeValue = resultTable.querySelector('.income-value'),
+    monthsavingsValue = resultTable.querySelector('.monthsavings-value'),
+    yearsavingsValue = resultTable.querySelector('.yearsavings-value');
+
+const expensesItems = document.querySelectorAll('input.expenses-item');
+
+const expensesItemBtn = document.querySelectorAll('button')[0],
+    optionalexpensesBtn = document.querySelectorAll('button')[1],
+    countBudgetBtn = document.querySelectorAll('button')[2];
+
+const optionalexpensesItems = document.querySelectorAll('.optionalexpenses-item');
+
+const incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
